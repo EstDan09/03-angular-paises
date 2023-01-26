fe55bfac454c57ccd53ecb4a1124bbb48cb1458c
Esteban Secaida <esecaidavargas@gmail.com>
2023-01-26 13:53:40 -0600

    final

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
 })
-export class VerPaisComponent {
+export class VerPaisComponent implements OnInit{
+
+  pais!: Country;
+
+  constructor (
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+    ) { } //suscribirse a cambios en la url
+
+  ngOnInit(): void {
+
+    this.activatedRoute.params.pipe(
+      switchMap( ({id}) => this.paisService.getPaisPorCodigoAlpha( id )),
+      tap( console.log)
+      )
+      .subscribe(pais => this.pais = pais);
+
+    // this.activatedRoute.params.subscribe( ({ id }) => {
+    //   console.log(id);
+
+    //   this.paisService.getPaisPorCodigoAlpha( id ).subscribe ( pais => {
+    //     console.log(pais);
+
+    //   })
+
+    // })
+  }
 
 }
